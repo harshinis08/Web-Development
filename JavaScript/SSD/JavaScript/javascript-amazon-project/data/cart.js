b4cbc0c8b8117a5e9cce7711cb4c5fe0c044e4b6
Harshini Sripuram <harshini.sripuram08@gmail.com>
2025-01-31 22:00:55 -0600

    Add removeFromCart / organize code ...

--- JavaScript/SSD/JavaScript/javascript-amazon-project/data/cart.js
+++ JavaScript/SSD/JavaScript/javascript-amazon-project/data/cart.js
@@ -1,1 +1,45 @@
-export let cart = [];
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 3,
+  },
+  {
+    productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    quantity: 1,
+  },
+];
+
+export function addToCart(productId) {
+  const quantitySelectorValue = Number(
+    document.querySelector(`.js-quantity-selector-${productId}`).value
+  );
+
+  let matchingItem = "";
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId === productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += quantitySelectorValue;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: quantitySelectorValue,
+    });
+  }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+}
